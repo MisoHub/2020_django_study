538fca31086df2e7588738bcab63635edd523fd5
DESKTOP-PREEAR2\Desmond <wantkps@gmail.com>
2020-01-30 00:05:59 +0900

    add bootstrap with 'https://bootswatch.com/superhero/'

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=60)
     content = models.TextField()
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
