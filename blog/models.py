85a0e8b9f02c83970c8e0eb818b0c79c05bb281d
DESKTOP-PREEAR2\Desmond <wantkps@gmail.com>
2020-02-01 00:07:57 +0900

    add ImageField with random image (+ add media url mapping statements)

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=60)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to='blog/%Y%m%d', blank=True)
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
