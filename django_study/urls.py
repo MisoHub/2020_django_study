fb23e06e5176e26fa71af285fcb15b4750378add
DESKTOP-PREEAR2\Desmond <wantkps@gmail.com>
2020-02-27 23:02:28 +0900

    add google login/logout use font awesome

--- django_study/urls.py
+++ django_study/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('',  include('blog.urls')),
     path('blog/',  include('blog.urls')),
     path('admin/', admin.site.urls),
     url('^markdownx/', include('markdownx.urls')),
